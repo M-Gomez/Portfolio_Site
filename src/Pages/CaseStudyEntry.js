4ee9c8d7435b52d456183b4f26f8c423b28d48a6
M Gomez <mgomez4th@gmail.com>
2024-04-22 05:07:27 -0500

    jenkins test commit 2

--- src/Pages/CaseStudyEntry.js
+++ src/Pages/CaseStudyEntry.js
@@ -77,6 +77,9 @@ function CaseStudyEntry() {
                 {isViewerOpen && (
                     <ImageViewer
                         src={images}
+
+
+
                         currentIndex={currentImage}
                         onClose={closeImageViewer}
                         disableScroll={false}
